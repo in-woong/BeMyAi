33fb63b948706deff21918b15a888b94bdea98ce
woong <inwoong100@gmail.com>
2025-01-28 15:20:53 +0900

    Initial commit for monorepo-backend

---
+++ backend/src/main/java/BeMyAi/woong/BeMyAiProjectApplication.java
@@ -0,0 +1,13 @@
+package BeMyAi.woong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMyAiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMyAiProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/BeMyAi/woong/BeMyAiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package BeMyAi.woong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMyAiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
